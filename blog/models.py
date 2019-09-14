71cc05d10ba9d5c010400fba93350fb5de6cf275
Dev Ryu <eostmghf01@gmail.com>
2019-09-14 09:31:58 +0900

    6-7
    comment model,admin

--- blog/models.py
+++ blog/models.py
@@ -12,3 +12,15 @@ class Post(BaseModel):
     def __str__(self):
         return '%s - %s' % (self.id, self.title)
     # 쿼리셋 리턴값 문자로 변환되게 (어드민에서 에서 실제 {{}}형식이 아닌텍스트로)정의
+
+
+class Comment(BaseModel):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    # 댓글달릴 글의 외래키
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # 댓글단 유저의 외래키
+    content = models.TextField()
+    # 댓글 내용
+
+    def __str__(self):
+        return '%s - %s' % (self.id, self.user)
