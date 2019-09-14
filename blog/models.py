684a7e11cfe1a0b6c122f947ba0b8afb45225309
Dev Ryu <eostmghf01@gmail.com>
2019-09-14 10:29:00 +0900

    7 all about

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from helper.models import BaseModel
 from users.models import User
+# from taggit.managers import TaggableManager
 
 
 class Post(BaseModel):
@@ -21,6 +22,18 @@ class Comment(BaseModel):
     # 댓글단 유저의 외래키
     content = models.TextField()
     # 댓글 내용
+    image = models.ImageField(blank=True, null=True)
+    # 차이점은: Null의 경우 DB와 관련이 되어있고 데이터 베이이스 컬럼이 널을 가질건지 아닌지 가진다
+    # blank의 경우 유효성검사에서 할지 안할지
+    likes = models.ManyToManyField(User, related_name='likes', blank=True)
+    # 유저클래스와 이어서 여러유저가 여러라이크를 누르는데 토글로 막을거임
+    # 어터케 M대M이쥬? M대1아닌가 각 유저들당 하나의 좋아요만 누르면되는거 아님?
+    # tags = TaggableManager()
+    #
 
     def __str__(self):
         return '%s - %s' % (self.id, self.user)
+
+    def total_likes(self):
+        return self.likes.count()
+        # 총 좋아요 카운트
--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.template.loader import render_to_string
-from blog.models import Post
+from blog.models import Post, Comment
 
 # 모델에서 포스트를 가져옴
 
@@ -18,10 +18,32 @@ def posts_list(request):
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-
-    return render(request, 'blog/post_detail.html', context={'post': post})
-
+    comments = Comment.objects.filter(post=post.id)
+    # 코멘트의 오브젝트를 게시글 번호로 가져온다
+    is_liked = False
+    # 기본적으로 좋아요는 false
+    if post.likes.filter(id=request.user.id).exists():
+        is_liked = True
+    # 포스트 라이크를 누르면 유저 아이디가 존재하는지 필터링하고 있으면 좋아요는 true가됨
+    return render(request, 'blog/post_detail.html', context={'post': post, 'comments': comments, 'is_liked': is_liked, 'total_likes': post.total_likes()})
+    # 리턴해줌
 # @login_required
+# @require_POST
+# 인증된 유저만 좋아요를 누름
+
+
+def post_like(request):
+    post = get_object_or_404(Post, id=request.POST.get('post_id'))
+    # 장고모델 파라미터받고 객체가 존재하지 않으면 에러발생
+    is_liked = post.likes.filter(id=request.user.id).exists()
+
+    if is_liked:
+        post.likes.remove(request.user)
+    # 라이크가 있으면 라이크를 지우고
+    else:
+        post.likes.add(request.user)
+    # 라이크가 기존에 없으면 추가해줘라
+    return HttpResponseRedirect(reverse('post_detail', kwargs={'post_id': post.id}))
 
 
 def post_write(request):
@@ -46,3 +68,11 @@ def post_write(request):
             # /post/%d/ % post.id == reverse('post_detail', kwargs= [post.id, article.id]}
             # 여러개받을때는 kwargs가 나음 키벨류형태에서 에러가 안남 args는 하나씩만 받음
     return render(request, 'blog/post_write.html', {'user': request.user, 'errors': errors})
+
+
+def comment_write(request):
+    pass
+
+
+# def like_toggle(request):
+#     pass
